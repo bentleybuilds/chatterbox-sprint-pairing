7191f94421dd56c7c04d299ec50ae417057be5c7
pwrmvspark <danielparkonline@gmail.com>
2019-06-07 18:59:09 -0700

    draft_commit_chatterBox

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -36,3 +36,78 @@ var App = {
     FormView.setStatus(false);
   }
 };
+
+
+
+// var App = {
+
+//   $spinner: $('.spinner img'),
+
+//   username: 'anonymous',
+
+//   initialize: function() {
+//     App.username = window.location.search.substr(10);
+
+//     FormView.initialize();
+//     FormView.setUser(App.username);
+//     RoomsView.initialize();
+//     MessagesView.initialize();
+
+
+//     // Fetch initial batch of messages
+//     App.startSpinner();
+//     App.fetch(App.stopSpinner);
+
+
+//     App.refresher = $('.submit').click(function() {
+//       FormView.handleSubmit(App.stopSpinner);
+//       MessagesView.clearMessages();
+//       App.startSpinner();
+//       App.fetch(App.stopSpinner);
+//     });
+
+//     RoomsView.$select.change(function() {
+//       App.changeRoom(this.value);
+//     });
+//   },
+
+//   fetch: function(callback = ()=>{}) {
+//     Parse.readAll((data) => {
+//       // examine the response from the server request:
+//       console.log(data);
+//       for (var i = 0; i < data.results.length; i++) {
+//         if (data.results[i].text) {
+//           var usr = data.results[i].username;
+//           var msg = data.results[i].text;
+//           // if not include dont append
+//           // hacker defense
+//           if (!msg.includes("<")) {
+//             $('#chats').append(`<div class="chat"><div class="username">${usr}</div>
+//             <div>${msg}</div></div>`);
+//           } 
+//         }
+//         // console.log('working');
+//       }
+//       callback();
+//     });
+//   },
+
+//   changeRoom: function(roomName) {
+//     MessagesView.clearMessages();
+//     window.filter = roomName.toLowerCase();
+//     App.startSpinner();
+//     App.fetch(App.stopSpinner);
+//   },
+
+//   startSpinner: function() {
+//     App.$spinner.show();
+//     FormView.setStatus(true);
+//   },
+
+//   stopSpinner: function() {
+//     App.$spinner.fadeOut('fast');
+//     FormView.setStatus(false);
+//   },
+
+// };
+
--- client/scripts/formView.js
+++ client/scripts/formView.js
@@ -1,3 +1,35 @@
+// var FormView = {
+
+//   $form: $('form'),
+//   currentUser: null,
+
+//   initialize: function() {
+//     FormView.$form.on('submit', FormView.handleSubmit);
+//   },
+
+//   setUser: function(username) {
+//     FormView.currentUser = username;
+//   },
+
+//   handleSubmit: function(event) {
+//     // Stop the browser from submitting the form
+//     var msgText = $('#message').val();
+//     var msg = {username: FormView.currentUser, text: msgText, roomname: window.filter};
+    
+//     Parse.create(msg, (data) => {
+//       console.log(data);
+//     });
+
+//     $('#refresh').click();
+//   },
+
+//   setStatus: function(active) {
+//     var status = active ? 'true' : null;
+//     FormView.$form.find('input[type=submit]').attr('disabled', status);
+//   }
+
+// };
+
 var FormView = {
 
   $form: $('form'),
